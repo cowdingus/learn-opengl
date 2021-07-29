332dbe4aa2efbf6ff984ca2d1869bc18bbc22874
Ooflamp <zaki.nadhif45@gmail.com>
2021-07-29 16:53:05 +0700

    Add colors to triangle vertex data and points VertexAttrib appropriately

--- code/main.cpp
+++ code/main.cpp
@@ -60,9 +60,10 @@ int main()
 
 	/******************** Triangle ********************/
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		// positions         // colors
+		0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f    // top
 	};
 
 	// Make a VBO object
@@ -78,8 +79,11 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0);
 	/******************** Triangle ********************/
