bca20124d05c75cea3c26d05dfdeee33b1f6b5a8
cowdingus <56494343+cowdingus@users.noreply.github.com>
2021-08-02 20:23:26 +0700

    Edit vertex and fragment shader to handle texture coordinates

--- assets/shaders/fragmentShader.glsl
+++ assets/shaders/fragmentShader.glsl
@@ -1,9 +1,17 @@
 #version 330 core
 
 in vec3 ourColor;
+in vec2 ourTexCoord;
+
 out vec4 FragColor;
+
+uniform sampler2D ourTexture;
 
 void main()
 {
-	FragColor = vec4(ourColor.rgb, 1.0f);
+	vec4 texel = texture(ourTexture, ourTexCoord);
+	if (texel.xyz == vec3(0.0f, 0.0f, 0.0f))
+		FragColor = vec4(ourColor, 1.0f);
+	else
+		FragColor = texture(ourTexture, ourTexCoord) * vec4(ourColor, 1.0f);
 }
