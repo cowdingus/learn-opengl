d1357aaec86befcbeef79de4bfa01e2abee1185f
cowdingus <56494343+cowdingus@users.noreply.github.com>
2021-08-15 08:41:56 +0700

    Add matrix 4 uniform setter on Shader class

--- code/Shader.cpp
+++ code/Shader.cpp
@@ -2,6 +2,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <fstream>
 #include <iostream>
@@ -98,4 +99,9 @@ void Shader::setInt(const std::string &name, int value) const
 void Shader::setFloat(const std::string &name, float value) const
 {
 	glUniform1f(glGetUniformLocation(m_id, name.c_str()), value);
+}
+
+void Shader::setMatrix4(const std::string& name, const glm::mat4& matrix) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE, glm::value_ptr(matrix));
 }
--- code/Shader.hpp
+++ code/Shader.hpp
@@ -2,6 +2,8 @@
 
 #include <string>
 
+#include <glm/matrix.hpp>
+
 class Shader
 {
 public:
@@ -13,6 +15,7 @@ public:
 	void setBool(const std::string& name, bool value) const;
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
+	void setMatrix4(const std::string& name, const glm::mat4& matrix) const;
 private:
 	unsigned int m_id;
 
